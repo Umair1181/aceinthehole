88e3a12f95c968cec63ddd7732dc62930c36ae7d
Hanzla Naveed <mhanzlanaveed@gmail.com>
2020-03-27 15:23:19 +0500

    F

--- API/emailSend.js
+++ API/emailSend.js
@@ -1,9 +1,16 @@
 var nodemailer = require("nodemailer");
 
 module.exports = transporter = nodemailer.createTransport({
+  host: "host",
+  port: 25,
+  secure: false,
   service: "gmail",
   auth: {
     user: "mhanzlanaveed@gmail.com",
     pass: "mhanzlanaveed@12345"
+  },
+  tls: {
+    // do not fail on invalid certs
+    rejectUnauthorized: false
   }
 });

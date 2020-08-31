8ef1ebf690789ad5840bb84cd94a028b85322f6e
Hanzla Naveed <mhanzlanaveed@gmail.com>
2020-08-31 12:11:47 +0500

    F

--- API/emailSend.js
+++ API/emailSend.js
@@ -6,8 +6,18 @@ module.exports = transporter = nodemailer.createTransport({
   // secure: false,
   service: "gmail",
   auth: {
-    user: "aceintheholeapp@gmail.com",
-    pass: "Aith2020",
+    // user: "aceintheholeapp@gmail.com",
+    // pass: "Aith2020",
+    XOAuth2: {
+      user: "aceintheholeapp@gmail.com",
+      clientId:
+        "968139947176-bkquv3cs2nni3rag1bpr9pspffct4is4.apps.googleusercontent.com",
+      clientSecret: "5Y0rtCEMiHUDe5DS-oxHlNSZ",
+      accessToken:
+        "ya29.a0AfH6SMBrC8LKZ2zNGxuomOv7e8m1n6kkdoFYlkgWVlpURLyZPSKKoVXOMN_4-hCLHid2jnL0n_7uCS3LDHNDPFmAiR19Qn9lqkTrBKBxbEJU3k2oRSm-DKNCEemSO8Y8yApx9cLrmMZV-XMwUEJnXxwjec5E2Qg3SrM",
+      refreshToken:
+        "1//04vE1nYuvoB7fCgYIARAAGAQSNwF-L9Ir8NhDc98maQAf92b8ngRMmKkmCWm3Al8h5dkpAmc2yrI284NteDq470SDuuDnO5jnC6Y",
+    },
   },
   tls: {
     // do not fail on invalid certs
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// const xoauth2 = require("xoauth2");
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");

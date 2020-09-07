e8556cfb21bee221dd84be0f8eac879c642188cd
Hanzla Naveed <mhanzlanaveed@gmail.com>
2020-09-07 12:53:22 +0500

    F

--- CONFIG/dbConfig.js
+++ CONFIG/dbConfig.js
@@ -1,4 +1,25 @@
 module.exports = {
   mongodbOnline:
-    "mongodb+srv://AceInAHoleDB:AceInAHoleDB@cluster0-erx7v.mongodb.net/test?retryWrites=true&w=majority"
+    "mongodb+srv://AceInAHoleDB:AceInAHoleDB@cluster0-erx7v.mongodb.net/test?retryWrites=true&w=majority",
 };
+
+/*
+
+  /////GENERAL INFORMATION/////
+
+   *****Nodemailer Credentials*****
+   user: "info.aceinthehole@gmail.com",
+   pass: '!)(090)"!@#!!'
+
+   *****Firebase Credentials*****
+
+
+
+   *****Heroku Credentials*****
+    Email:mhanzlanaveed@gmail.com
+
+
+   *****MongoDB Credentials*****
+    Email:mhanzlanaveed@gmail.com
+   
+*/

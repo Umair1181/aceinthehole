af2fe76168bdeb04bf0712ddb8352e0011319b72
Umair maqbool <umairmaqbool.1181@gmail.com>
2020-10-11 18:33:59 +0500

    fix

--- server.js
+++ server.js
@@ -57,64 +57,135 @@ mongoose
   });
 
 ////////////STRIPE APIs///////////////
+
+const getStripeID =  ( ) => {
+  return new Promise( async ( resolve, reject )=> {
+    const account = await stripe.accounts.create({
+      country: 'US',
+      type: 'express',
+      requested_capabilities: ['card_payments', 'transfers'],
+    });
+    if( account ){
+      resolve( account )
+    }else{
+      reject({ message: "Catch Error Stripe Account Id", success :false });
+    }
+  })
+  
+  // return res.json({ account }).status ( 200 );
+}
+
+const cretateAccountLink  = ( aceinholeId , stripeId) => {
+  return new Promise(async ( resolve, reject ) => {
+    let accountLink = await stripe.accountLinks.create({
+      account: stripeId,
+      refresh_url: 'https://ace-in-the-hole.herokuapp.com/connectivity/link/' + aceinholeId + '/' + stripeId,
+      return_url: 'https://ace-in-the-hole.herokuapp.com/connectivity/success/' + aceinholeId + '/' + stripeId,
+      type: 'account_onboarding',
+      collect: 'eventually_due'
+    });
+    if( accountLink ){
+      accountLink = { ...accountLink, state , success: true, message: "Account Link"}
+      resolve( accountLink );
+    }else{
+      reject({ message: "Catch Error Account Link Creation", success: false });
+    }
+  })
+}
 app.post("/connect", (req, res) => {
   let { sellerID } = req.body;
-  if (sellerID === "") {
+  if (sellerID === "" || sellerID === undefined) {
     return res.json({ msg: "Invalid Seller ID", success: false }).status(505);
   }
-  const state = sellerID;
-  // req.session.state = state;
-  const suggested_capabilities = ["card_payments", "transfers"];
-  const stripe_user = [{ email: "test@gmail.com" }];
-  const args = new URLSearchParams({ state, client_id: process.env.client_id });
-  const url = `https://connect.stripe.com/express/oauth/authorize?${args.toString()}&suggested_capabilities[]=transfers&suggested_capabilities[]=card_payments`;
-  res.send({ url, state });
+  getStripeID(  )
+  .then( resp_getStripeId => {
+    // return res.json({ resp_getStripeId });
+      cretateAccountLink(resp_getStripeId.id, sellerID)
+      .then( resp_cretateAccountLink => {
+        return res.json( resp_cretateAccountLink ).status( 200 );
+      })
+      .catch( err => {
+          return res.json(err).status( 500 );
+      })
+  } )
+  .catch( err => {
+    return res.json( err ).status( 500 );
+  } )
+  // const state = sellerID;
+  // // req.session.state = state;
+  // const suggested_capabilities = ["card_payments", "transfers"];
+  // const stripe_user = [{ email: "test@gmail.com" }];
+  // const args = new URLSearchParams({ state, client_id: process.env.client_id });
+  // const url = `https://connect.stripe.com/express/oauth/authorize?${args.toString()}&suggested_capabilities[]=transfers&suggested_capabilities[]=card_payments`;
+  // res.send({ url, state });
 });
 
 app.post("/connect/oauth", async (req, res) => {
   console.log("oauth # 1");
-  // return res.json ({ msg: "oath" });
-  let { code, state } = req.body;
+  // return res.json ({ msg: "oauth" });
+  let errorMessage = false;
+  const  { aceinholeId, stripeId } = req.body;
+  if( aceinholeId === "" || aceinholeId === undefined )  {
+    errorMessage = "Invalid Ace In Hole Id";
+  }else if( stripeId === "" || stripeId === undefined ){
+    errorMessage = "Invalid Stripe Id";
+  }else{
+    errorMessage = false;
+  }
+
+  if( errorMessage === false ){
+    Seller.updateOne({ _id: aceinholeId }, { stripeAccountId: stripeId })
+    .then( resp_updated => {
+      // console.log( resp_updated );
+      return res.json({ message: "Connected Successfully", success: true }).status( 200 );
+    }) 
+    .catch( err => {
+      return res.json({ message: "Catch Error, Seller Finding", success: false }).status( 500 );
+    })
+  }else{
+    return res.json({ message: errorMessage, success: false }).status( 500 );
+  } 
+});
   // sellerId = state
   // Send the authorization code to Stripe's API.
-  stripe.oauth
-    .token({
-      grant_type: "authorization_code",
-      code,
-    })
-    .then(
-      (response) => {
-        console.log("oauth # 2");
-        // response.stripe_user_id seller stripe connect account id
+//   stripe.oauth
+//     .token({
+//       grant_type: "authorization_code",
+//       code,
+//     })
+//     .then(
+//       (response) => {
+//         console.log("oauth # 2");
+//         // response.stripe_user_id seller stripe connect account id
 
-        let connected_account_id = response.stripe_user_id;
-        saveAccountId(connected_account_id, state)
-          .then((result) => {
-            return res.json(result).status(200);
-          })
-          .catch((err) => {
-            return res.json(err).status(500);
-          });
-        // Render some HTML or redirect to a different page.
-      },
-      (err) => {
-        console.log("oauth # 3");
+//         let connected_account_id = response.stripe_user_id;
+//         saveAccountId(connected_account_id, state)
+//           .then((result) => {
+//             return res.json(result).status(200);
+//           })
+//           .catch((err) => {
+//             return res.json(err).status(500);
+//           });
+//         // Render some HTML or redirect to a different page.
+//       },
+//       (err) => {
+//         console.log("oauth # 3");
 
-        if (err.type === "StripeInvalidGrantError") {
-          console.log("oauth # 4");
-          return res.status(400).json({
-            success: false,
-            msg: "Invalid authorization code: " + code,
-          });
-        } else {
-          console.log("oauth # 5");
-          return res
-            .status(500)
-            .json({ success: false, msg: "An unknown error occurred." });
-        }
-      }
-    );
-});
+//         if (err.type === "StripeInvalidGrantError") {
+//           console.log("oauth # 4");
+//           return res.status(400).json({
+//             success: false,
+//             msg: "Invalid authorization code: " + code,
+//           });
+//         } else {
+//           console.log("oauth # 5");
+//           return res
+//             .status(500)
+//             .json({ success: false, msg: "An unknown error occurred." });
+//         }
+//       }
+//     );
+// });
 
 const saveAccountId = (stripeId, sellerID) => {
   return new Promise((resolve, reject) => {
@@ -181,6 +252,8 @@ app.post("/api/create-intent", async (req, res) => {
     }
   );
 });
+
+
 app.post("/transfer-to-account", async (req, res) => {
   const { sellerConnectAccountId, sellerId, orderId, amount } = req.body;
 
@@ -404,8 +477,8 @@ app.post("/retrieve-stripe-connect-account", (req, res) => {
               }
               // return res.json({ stripeId: foundSeller.stripeAccountId });
               const paymentMethod = await stripe.accounts.retrieve(
-                // `${foundSeller.stripeAccountId}`
-                "acct_1HXiYtLBKJdxqCYO"
+                `${foundSeller.stripeAccountId}`
+                // "acct_1HXiYtLBKJdxqCYO"
               );
 
               if( paymentMethod.default_currency !== "usd" ){
@@ -468,14 +541,7 @@ app.post("/stripe-account-link",async ( req, res ) => {
     });
     return res.json({ accountLink });
 })
-app.post( "/create-account" ,async ( req, res ) => {
-  const account = await stripe.accounts.create({
-    country: 'US',
-    type: 'express',
-    requested_capabilities: ['card_payments', 'transfers'],
-  });
-  return res.json({ account }).status ( 200 );
-} ) 
+
 // app.post("/upload", upload.array("image", 1), (req, res) => {
 //   let imageArrays = req.files;
 //   let ImageURLsArray = [];
